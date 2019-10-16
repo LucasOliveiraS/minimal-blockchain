3c11945a150e332c56174ff478fe157d4c38841f
Lucas Oliveira <oliveirasp6@gmail.com>
2019-10-16 00:58:53 -0300

    initial blockchain implementation

--- basic/blockchain.py
+++ basic/blockchain.py
@@ -62,4 +62,19 @@ blockchain = Blockchain()
 def mine_block():
     previous_block = blockchain.get_last_block()
     previous_proof = previous_block['proof']
-    
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message' : 'Congratulations, you just mined a block',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full chain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain' : blockchain.chain,
+                'lenght : ' len(blockchain.chain)}
+    return jsonify(response), 200
